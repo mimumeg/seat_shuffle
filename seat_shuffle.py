cc88325e4d0e0d3973be1bc0500f1c369f73fe78
mimumeg <meg.stella555@gmail.com>
2018-11-27 11:57:14 +0900

    members.txtを読み込んで、とりあえず１５人の名前を取得することができた。

--- seat_shuffle.py
+++ seat_shuffle.py
@@ -1,6 +1,15 @@
 def main():
-    pass
+    # members.txtを開く
+    f = open("members.txt", mode="r")
 
+    # 参加者名を読み込む
+    name = f.read()
+
+    # 参加者の名前をとりあえず表示させたい
+    print(name)
+
+    # ファイルを閉じる
+    f.close()
 
 if __name__ == "__main__":
     main()
