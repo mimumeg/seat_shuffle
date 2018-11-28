5bb26932b4add9b0920a5952bc4e9c9ca8eee038
mimumeg <meg.stella555@gmail.com>
2018-11-28 18:20:07 +0900

    リストへの追加:append()、リストからの削除:remove()を覚えた。 参加者から1人取り出し、参加者リストから取り出した1人を削除することができた。 あとは繰り返しすればできそう！

--- seat_shuffle.py
+++ seat_shuffle.py
@@ -9,6 +9,25 @@ def main():
         members_list = members_text.split("\n")
 
     # 参加者の名前をとりあえず表示させたい
+    # print(members_list)
+
+    # テーブルを３つ作成
+    table1 = []
+    table2 = []
+    table3 = []
+
+    # 15人分シャッフルするまで終了しない
+
+    # 参加者を1人シャッフル選出
+    results = random.choice(members_list)
+
+    # table1に格納
+    table1.append(results)
+
+    # 参加者リストから取り出した氏名を削除
+    members_list.remove(results);
+
+    print(table1)
     print(members_list)
 
 
